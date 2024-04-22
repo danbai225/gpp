aebb251e23284b8051460a5ddc7de8c18e43cf63
s0tephen <2477284535@qq.com>
2024-04-22 13:59:43 +0800

    simple page

--- client/frontend/src/components/Progress.vue
+++ client/frontend/src/components/Progress.vue
@@ -3,19 +3,22 @@
     <div class="center">
       <n-space vertical>
         <n-space>
-          <n-progress type="circle" :percentage="percentageRef"/>
+          <n-progress type="circle"
+                      :status="percentageRef<=25?'error':percentageRef<=50?'warning':percentageRef<=75?'info':'success'"
+                      :percentage="percentageRef">
+            {{ percentageRef === 100 ? '加速完成...' : percentageRef === 0 ? '未开始' : '正在加速' }}
+          </n-progress>
         </n-space>
         <n-space>
-          <n-button style="margin-left: 20px" @click="start">
+          <n-button v-if="!state" @click="start" style="margin-left: 20px">
             开始加速
           </n-button>
-          <n-button @click="stop">
+          <n-button v-else @click="stop" style="margin-left: 20px">
             结束加速
           </n-button>
         </n-space>
       </n-space>
     </div>
-
   </div>
 </template>
 
@@ -24,30 +27,34 @@ import {defineComponent, ref} from 'vue'
 import {Start, Stop} from "../../wailsjs/go/main/App";
 
 const percentageRef = ref(0)
-const add = () => {
-  percentageRef.value += 10
-  if (percentageRef.value > 100) {
-    percentageRef.value = 0
-  }
-}
+const state = ref(false)
 
 const start = () => {
+  setTimeout(() => {
+    let i = 0
+    const timer = setInterval(() => {
+      i += 20
+      percentageRef.value = i
+      if (i >= 100) {
+        clearInterval(timer)
+      }
+    }, 500)
+  }, 1000)
   Start().then(res => {
-    console.log('res:', res)
+    console.log('startRes', res)
+    if (res === 'ok') {
+      state.value = true
+    }
   })
 }
 const stop = () => {
   Stop().then(res => {
+    percentageRef.value = 0
+    state.value = false
     console.log('stopRes', res)
   })
 }
 
-const minus = () => {
-  percentageRef.value -= 10
-  if (percentageRef.value < 0) {
-    percentageRef.value = 100
-  }
-}
 
 </script>
 
