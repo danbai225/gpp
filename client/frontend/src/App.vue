383c4339f42dbba0d8a62b455c49d6fe2476750c
s0tephen <2477284535@qq.com>
2024-04-22 13:03:44 +0800

    debug

--- client/frontend/src/App.vue
+++ client/frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
 import Layout from "./components/Layout.vue";
 import Progress from "./components/Progress.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 
 <template>
 <!--  <Layout/>-->
   <Progress/>
+<!--  <HelloWorld/>-->
 </template>
 
 <style>
