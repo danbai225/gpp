77fec44949e3c2ec6dc7a442eef8a7b2c6656305
s0tephen <2477284535@qq.com>
2024-04-22 14:57:03 +0800

    加速中.

--- frontend/src/components/Progress.vue
+++ frontend/src/components/Progress.vue
@@ -10,11 +10,8 @@
           </n-progress>
         </n-space>
         <n-space>
-          <n-button v-if="!state" @click="start" style="margin-left: 20px">
-            开始加速
-          </n-button>
-          <n-button v-else @click="stop" style="margin-left: 20px">
-            结束加速
+          <n-button :disabled="btnDisabled" @click="!state?start():stop()" style="margin-left: 20px">
+            {{ btnText }}
           </n-button>
         </n-space>
       </n-space>
@@ -23,34 +20,35 @@
 </template>
 
 <script lang="ts" setup>
-import {defineComponent, ref} from 'vue'
+import {ref} from 'vue'
 import {Start, Stop} from "../../wailsjs/go/main/App";
 
 const percentageRef = ref(0)
 const state = ref(false)
+const btnText = ref('开始加速')
+const btnDisabled = ref(false)
 
 const start = () => {
-  setTimeout(() => {
-    let i = 0
-    const timer = setInterval(() => {
-      i += 20
-      percentageRef.value = i
-      if (i >= 100) {
-        clearInterval(timer)
-      }
-    }, 500)
-  }, 1000)
+  btnDisabled.value = true
+  btnText.value = '加速中.'
   Start().then(res => {
+    state.value = true
     console.log('startRes', res)
-    if (res === 'ok') {
-      state.value = true
-    }
+    let timer = setInterval(() => {
+      percentageRef.value += 10
+      if (percentageRef.value === 100) {
+        clearInterval(timer)
+        btnText.value = '结束加速'
+        btnDisabled.value = false
+      }
+    }, 100)
   })
 }
 const stop = () => {
   Stop().then(res => {
     percentageRef.value = 0
     state.value = false
+    btnText.value = '开始加速'
     console.log('stopRes', res)
   })
 }
