d7eaadfa356e33fc5d3cf630d0531c05b1d5390e
danbai <danbai@88.com>
2024-04-23 13:08:12 +0800

    rm io

--- main.go
+++ main.go
@@ -16,9 +16,6 @@ func main() {
 	if _, err := os.Stat(".dev"); err != nil {
 		if len(os.Args) == 1 {
 			command := elevate.Command(os.Args[0], "sudo")
-			command.Stderr = os.Stderr
-			command.Stdout = os.Stdout
-			command.Stdin = os.Stdin
 			_ = command.Run()
 			os.Exit(0)
 		}
