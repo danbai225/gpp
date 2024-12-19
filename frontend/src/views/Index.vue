bf8fe7e8ce01d619e0c0ce3af4081c0a53576b75
danbai <danbai@88.com>
2024-12-19 10:02:38 +0800

    fix null http proxy

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -51,7 +51,7 @@
           </n-button>
         </n-space>
         <n-gradient-text type="success" style="margin-left: 130px;margin-top: 35px">
-          v1.4.1
+          v1.4.3
         </n-gradient-text>
       </n-space>
       <div>
